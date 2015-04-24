b8689abe3c3f0080b601e76a155679792958d167
stevegordon <tdtsgordon@gmail.com>
2015-04-24 12:11:12 -0700

    add user password and authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: {maximum: 20}
-  validates :email, presence: true, length: {maximum: 52}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, uniqueness: { case_sensitive: false },
+   presence: true, length: {maximum: 52}, 
+      format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
+  validates :password, length: {minimum: 6}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
     def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -28,8 +28,36 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 41 + "@example.com"
     assert_not @user.valid?
   end
+
+  test "email validation should accept valid addresses" do 
+    valid_addresses = %w[user@example.com USER@gmail.com Tim_user@work.email.com Steve.Gordon@rails.io ann+bob@hotmail.jp]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+    assert @user.valid?, "#{valid_address.inspect} should be valid"
     
   end
+end
+  test "email validation should reject invalid addresses" do 
+    invalid_addresses = %w[phil@example,com Abc.example.com user.name@example. foobar@hotmail+gmail.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+    assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+end
+end
+
+  test "email addresses should be unique" do 
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+  test "password should have a minimum length" do 
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+    
+  end
+
+end
   # test "the truth" do
   #   assert true
   # end
