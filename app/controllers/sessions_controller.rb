030c3460cfd2370d99b949fbedeb1d8ae2765970
stevegordon <tdtsgordon@gmail.com>
2015-04-26 20:06:34 -0700

    finish log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,10 @@ class SessionsController < ApplicationController
       flash.now[:danger] = "Invalid e-mail and/or password"
       render 'new'
   end
+  end
   def destroy
+    log_out
+    redirect_to root_url
   end
 
 end
